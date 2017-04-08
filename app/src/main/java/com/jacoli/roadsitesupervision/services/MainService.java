a3de49b0b061837d1d9c35571cad0cd1f87e3a2d
jacoli <jaco.lcg@gmail.com>
2017-04-08 21:17:07 +0800

    update

--- app/src/main/java/com/jacoli/roadsitesupervision/services/MainService.java
+++ app/src/main/java/com/jacoli/roadsitesupervision/services/MainService.java
@@ -1,5 +1,7 @@
 package com.jacoli.roadsitesupervision.services;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
